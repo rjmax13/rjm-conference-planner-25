8a2bf09fcfae8d5a0bcfe250a1efdcb52bc892c8
rjmax13 <rod_max@sbcglobal.net>
2025-03-03 13:14:42 -0500

    first commit message

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -7,6 +7,7 @@ const ConferenceEvent = () => {
     const [showItems, setShowItems] = useState(false);
     const [numberOfPeople, setNumberOfPeople] = useState(1);
     const venueItems = useSelector((state) => state.venue);
+    const avItems = useSelector((state) => state.av);
     const dispatch = useDispatch();
     const remainingAuditoriumQuantity = 3 - venueItems.find(item => item.name === "Auditorium Hall (Capacity:200)").quantity;
 
@@ -18,7 +19,7 @@ const ConferenceEvent = () => {
 
     const handleAddToCart = (index) => {
         if (venueItems[index].name === "Auditorium Hall (Capacity:200)" && venueItems[index].quantity >= 3) {
-          return; 
+          return; // Prevent further additions
         }
         dispatch(incrementQuantity(index));
       };
@@ -157,6 +158,20 @@ const ConferenceEvent = () => {
 
                                 </div>
                                 <div className="addons_selection">
+                                    {avItems.map((item, index) =>(
+                                        <div className="av_data venue_main" key={index}>
+                                            <div className="img">
+                                                <img src="{item.img}" alt="{item.name}" />
+                                            </div>
+                                            <div className="text"> {item.name} </div>
+                                            <div> ${item.cost} </div>
+                                            <div className="addons_btn">
+                                                <button className="btn-warning" onClick={() => handleDecrementAvQuantity(index) }> &ndash </button>
+                                                <span className="quantity-value"> {item.quantity} </span>
+                                                <button className="btn-success" onClick={()=>handleIncrementAvQuantity} > &#43 </button>
+                                            </div>
+                                        </div>
+                                    ))}
 
                                 </div>
                                 <div className="total_cost">Total Cost:</div>
